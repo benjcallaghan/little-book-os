164cf23eb253a509fcdf82b1dfef39a2420ec655
Ben Callaghan <benj.callaghan@gmail.com>
2025-04-28 23:07:29 -0600

    added C entry point and build tools

--- loader.s
+++ loader.s
@@ -5,12 +5,12 @@ FLAGS equ 0x0 ; multiboot flags
 CHECKSUM equ -MAGIC_NUMBER ; calculate the checksum (magic number + checksum + flags should equal 0)
 KERNEL_STACK_SIZE equ 4096 ; size of stack in bytes
 
-section .bss:
+section .bss
 align 4 ; align at 4 bytes
 kernel_stack: ; label points to beginning of memory
   resb KERNEL_STACK_SIZE ; reserve stack for the kernel
 
-section .text: ; start of the text (code) section
+section .text ; start of the text (code) section
 align 4 ; the code must be 4 byte aligned
   dd MAGIC_NUMBER ; write the magic number to the machine code,
   dd FLAGS ; the flags,
