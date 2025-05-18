f052a18a0dee0a69f17c8a94f9b56797bae7b567
Ben Callaghan <benj.callaghan@gmail.com>
2025-05-17 20:24:57 -0600

    replaced integer offset with a typed function pointer

--- src/interrupts.c
+++ src/interrupts.c
@@ -4,9 +4,16 @@
 #define MAX_INTERRUPTS 1
 #define CODE_SEGMENT 0x08
 
+struct interrupt_frame
+{
+    unsigned long eflags;
+    unsigned long cs;
+    unsigned long eip;
+};
+
 struct interrupt_descriptor
 {
-    unsigned long offset;
+    void (*handler)(struct interrupt_frame const *);
     unsigned short segment_selector;
     enum interrupt_gate_type gate_type;
     unsigned char privilege_level;
@@ -17,22 +24,15 @@ struct interrupt_descriptor_table table = { .size = sizeof interrupts, .interrup
 
 void load_interrupt_descriptor(struct interrupt_descriptor const *descriptor, struct interrupt_descriptor_unsafe *target)
 {
-    target->offset_low = descriptor->offset & 0xFFFF;
+    target->offset_low = (unsigned long)descriptor->handler & 0xFFFF;
     target->segment_selector = descriptor->segment_selector;
     target->gate_type = descriptor->gate_type;
     target->privilege_level = descriptor->privilege_level;
     target->present = true;
-    target->offset_high = (descriptor->offset >> 16) & 0xFFFF;
+    target->offset_high = ((unsigned long)descriptor->handler >> 16) & 0xFFFF;
 }
 
-struct interrupt_frame
-{
-    unsigned long eflags;
-    unsigned long cs;
-    unsigned long eip;
-};
-
-__attribute__((interrupt)) void interrupt_0_handler(__attribute__((unused)) struct interrupt_frame *frame)
+__attribute__((interrupt)) void interrupt_0_handler(__attribute__((unused)) struct interrupt_frame const *frame)
 {        
     serial_write("Someone tried to divide by zero.\n");
 }
@@ -40,7 +40,7 @@ __attribute__((interrupt)) void interrupt_0_handler(__attribute__((unused)) stru
 void initialze_interrupts()
 {
     struct interrupt_descriptor interrupt_0 = {
-        .offset = (unsigned long)&interrupt_0_handler,
+        .handler = interrupt_0_handler,
         .segment_selector = CODE_SEGMENT,
         .gate_type = interrupt_32,
         .privilege_level = 0
