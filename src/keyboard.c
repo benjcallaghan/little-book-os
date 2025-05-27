99882041f6bd9a92fe4515425ce8750f13744e5d
Ben Callaghan <benj.callaghan@gmail.com>
2025-05-26 20:56:39 -0600

    removed ps/2 self test

--- src/keyboard.c
+++ src/keyboard.c
@@ -100,25 +100,15 @@ int initialize_keyboard()
     serial_write("Flushing the PS/2 output buffer.\n");
     inb(controller_data_port);
 
-    // Configure the controller with interrupts and translation disabled, and with clocks enabled.
+    // Disable interrupts and translation during init.
     serial_write("Configuring the PS/2 controller.\n");
     outb(controller_command_port, read_first_byte);
     uint8_t configuration = read_controller_response();
     configuration &= ~first_port_interrupt;
     configuration &= ~second_port_interrupt;
     configuration &= ~first_port_clock_disabled;
-    configuration &= ~first_port_translation;
     outb(controller_command_port, write_first_byte);
     write_controller_data(configuration);
-
-    // Make the controller perform a self-test.
-    outb(controller_command_port, test_controller);
-    uint8_t test_result = read_controller_response();
-    if (test_result != 0x55)
-    {
-        printf(serial_write_char, "Expected PS/2 controller to return a success test result. Received %X.\n", test_result);
-        return 1;
-    }
 
     return 0;
 }
