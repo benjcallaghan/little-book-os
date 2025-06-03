0a5877df98f750e31803deed1d49165938c2cbbb
Ben Callaghan <benj.callaghan@gmail.com>
2025-06-02 22:14:17 -0600

    converted scan codes to key codes

--- src/keyboard.c
+++ src/keyboard.c
@@ -68,6 +68,121 @@ enum keyboard_command : uint8_t
     reset_and_test = 0xFF,
 };
 
+enum scan_code_set_1
+{
+    KEY_NONE = 0x00,
+    KEY_ESCAPE = 0x01,
+    KEY_1 = 0x02,
+    KEY_2 = 0x03,
+    KEY_3 = 0x04,
+    KEY_4 = 0x05,
+    KEY_5 = 0x06,
+    KEY_6 = 0x07,
+    KEY_7 = 0x08,
+    KEY_8 = 0x09,
+    KEY_9 = 0x0A,
+    KEY_0 = 0x0B,
+    KEY_MINUS = 0x0C,
+    KEY_EQUAL = 0x0D,
+    KEY_BACKSPACE = 0x0E,
+    KEY_TAB = 0x0F,
+    KEY_Q = 0x10,
+    KEY_W = 0x11,
+    KEY_E = 0x12,
+    KEY_R = 0x13,
+    KEY_T = 0x14,
+    KEY_Y = 0x15,
+    KEY_U = 0x16,
+    KEY_I = 0x17,
+    KEY_O = 0x18,
+    KEY_P = 0x19,
+    KEY_LEFT_BRACKET = 0x1A,
+    KEY_RIGHT_BRACKET = 0x1B,
+    KEY_ENTER = 0x1C,
+    KEY_LEFT_CTRL = 0x1D,
+    KEY_A = 0x1E,
+    KEY_S = 0x1F,
+    KEY_D = 0x20,
+    KEY_F = 0x21,
+    KEY_G = 0x22,
+    KEY_H = 0x23,
+    KEY_J = 0x24,
+    KEY_K = 0x25,
+    KEY_L = 0x26,
+    KEY_SEMICOLON = 0x27,
+    KEY_APOSTROPHE = 0x28,
+    KEY_GRAVE = 0x29,
+    KEY_LEFT_SHIFT = 0x2A,
+    KEY_BACKSLASH = 0x2B,
+    KEY_Z = 0x2C,
+    KEY_X = 0x2D,
+    KEY_C = 0x2E,
+    KEY_V = 0x2F,
+    KEY_B = 0x30,
+    KEY_N = 0x31,
+    KEY_M = 0x32,
+    KEY_COMMA = 0x33,
+    KEY_DOT = 0x34,
+    KEY_SLASH = 0x35,
+    KEY_RIGHT_SHIFT = 0x36,
+    KEY_KEYPAD_STAR = 0x37,
+    KEY_LEFT_ALT = 0x38,
+    KEY_SPACE = 0x39,
+    KEY_CAPS_LOCK = 0x3A,
+    KEY_F1 = 0x3B,
+    KEY_F2 = 0x3C,
+    KEY_F3 = 0x3D,
+    KEY_F4 = 0x3E,
+    KEY_F5 = 0x3F,
+    KEY_F6 = 0x40,
+    KEY_F7 = 0x41,
+    KEY_F8 = 0x42,
+    KEY_F9 = 0x43,
+    KEY_F10 = 0x44,
+    KEY_NUM_LOCK = 0x45,
+    KEY_SCROLL_LOCK = 0x46,
+    KEY_KEYPAD_7 = 0x47,
+    KEY_KEYPAD_8 = 0x48,
+    KEY_KEYPAD_9 = 0x49,
+    KEY_KEYPAD_MINUS = 0x4A,
+    KEY_KEYPAD_4 = 0x4B,
+    KEY_KEYPAD_5 = 0x4C,
+    KEY_KEYPAD_6 = 0x4D,
+    KEY_KEYPAD_PLUS = 0x4E,
+    KEY_KEYPAD_1 = 0x4F,
+    KEY_KEYPAD_2 = 0x50,
+    KEY_KEYPAD_3 = 0x51,
+    KEY_KEYPAD_0 = 0x52,
+    KEY_KEYPAD_DOT = 0x53,
+    KEY_F11 = 0x57,
+    KEY_F12 = 0x58,
+
+    // Extended keys (0xE0 prefix)
+    KEY_KEYPAD_ENTER = 0xE01C,
+    KEY_RIGHT_CTRL = 0xE01D,
+    KEY_KEYPAD_SLASH = 0xE035,
+    KEY_RIGHT_ALT = 0xE038,
+    KEY_HOME = 0xE047,
+    KEY_UP_ARROW = 0xE048,
+    KEY_PAGE_UP = 0xE049,
+    KEY_LEFT_ARROW = 0xE04B,
+    KEY_RIGHT_ARROW = 0xE04D,
+    KEY_END = 0xE04F,
+    KEY_DOWN_ARROW = 0xE050,
+    KEY_PAGE_DOWN = 0xE051,
+    KEY_INSERT = 0xE052,
+    KEY_DELETE = 0xE053,
+    KEY_LEFT_GUI = 0xE05B,
+    KEY_RIGHT_GUI = 0xE05C,
+    KEY_APPS = 0xE05D,
+};
+
+struct scan_code_result
+{
+    enum scan_code_set_1 key;
+    bool pressed;
+};
+
 void reset_scan_code()
 {
     for (int i = 0; i < largest_scan_code; i++)
@@ -167,9 +282,40 @@ int initialize_keyboard()
     return 0;
 }
 
-void process_scan_code()
+void read_scan_code()
 {
+    struct scan_code_result result = {KEY_NONE, false};
 
+    if (scan_code_pos == 0)
+    {
+    }
+
+    // Single-byte scan code
+    else if (scan_code_pos == 1)
+    {
+        uint8_t code = in_progress_scan_code[0];
+        result.pressed = !(code & 0x80);
+        result.key = (enum scan_code_set_1)(code & 0x7F);
+    }
+
+    // Extended scan code
+    else if (scan_code_pos == 2 && in_progress_scan_code[0] == 0xE0)
+    {
+        uint8_t code = in_progress_scan_code[1];
+        result.pressed = !(code & 0x80);
+        result.key = (enum scan_code_set_1)(0xE000 | (code & 0x7F));
+    }
+
+    // TODO: Add Pause/Break and Print Screen
+
+    if (result.pressed)
+    {
+        printf(serial_write_char, "Key pressed %X\n", result.key);
+    }
+    else
+    {
+        printf(serial_write_char, "Key released %X\n", result.key);
+    }
 }
 
 __attribute__((interrupt, target("general-regs-only"))) void keyboard_interrupt_handler(__attribute__((unused)) struct interrupt_frame const *frame)
@@ -196,13 +342,7 @@ __attribute__((interrupt, target("general-regs-only"))) void keyboard_interrupt_
         else
         {
             // This is the end of the multi-byte scan code.
-            printf(serial_write_char, "Complete scan code ");
-            for (size_t i = 0; i < scan_code_pos; i++)
-            {
-                printf(serial_write_char, "%X", in_progress_scan_code[i]);
-            }
-            printf(serial_write_char, "\n");
-            process_scan_code();
+            read_scan_code();
             reset_scan_code();
         }
     }
@@ -214,9 +354,8 @@ __attribute__((interrupt, target("general-regs-only"))) void keyboard_interrupt_
     else
     {
         // This is a complete single-byte scan code.
-        printf(serial_write_char, "Complete scan code %X\n", scan_code);
         in_progress_scan_code[scan_code_pos++] = scan_code;
-        process_scan_code();
+        read_scan_code();
         reset_scan_code();
     }
 
