d9491c91628547c5d625e4f900051696942dbb49
Ben Callaghan <benj.callaghan@gmail.com>
2025-06-04 21:56:12 -0600

    added a sub-program as a boot module

--- src/loader.s
+++ src/loader.s
@@ -1,8 +1,9 @@
 global loader ; the entry symbol for ELF
 
 MAGIC_NUMBER equ 0x1BADB002 ; define the magic number constant
-FLAGS equ 0x0 ; multiboot flags
-CHECKSUM equ -MAGIC_NUMBER ; calculate the checksum (magic number + checksum + flags should equal 0)
+ALIGN_MODULES equ 0x00000001 ; tell GRUB to align modules at 4K boundaries
+MULTIBOOT_FLAGS equ ALIGN_MODULES
+CHECKSUM equ -(MAGIC_NUMBER + MULTIBOOT_FLAGS) ; calculate the checksum (magic number + checksum + flags should equal 0)
 KERNEL_STACK_SIZE equ 4096 ; size of stack in bytes
 
 extern kmain ; the function kmain is defined elsewhere
@@ -17,7 +18,7 @@ kernel_stack: ; label points to beginning of memory
 section .text ; start of the text (code) section
 align 4 ; the code must be 4 byte aligned
     dd MAGIC_NUMBER ; write the magic number to the machine code,
-    dd FLAGS ; the flags,
+    dd MULTIBOOT_FLAGS ; the flags,
     dd CHECKSUM ; and the checksum
 
 loader: ; the loader label (defined as entry point in linker script)
